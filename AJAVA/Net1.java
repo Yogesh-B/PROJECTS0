340a2580f161aed6ab8f58d420c65c82e192973d
Yogesh-B <yogeshbavishi199@gmail.com>
2020-04-19 19:59:33 +0530

    modified:   Web/index.html

--- AJAVA/Net1.java
+++ AJAVA/Net1.java
@@ -1,18 +1,18 @@
-/*
-Write a program to read name of a website from user and display its ip address
-
-*/
-import java.net.*;
-import java.io.*;
-
-public class Net1
-{
-	public static void main(String args[]) throws IOException
-	{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		String site=br.readLine();
-		InetAddress ip=InetAddress.getByName(site);		
-		//unknown host exception
-		System.out.println("\n\nIP address is:"+ip);
-	}
+/* 
+Write a program to read name of a website from user and display its ip address 
+ 
+*/ 
+import java.net.*; 
+import java.io.*; 
+ 
+public class Net1 
+{ 
+	public static void main(String args[]) throws IOException 
+	{ 
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
+		String site=br.readLine(); 
+		InetAddress ip=InetAddress.getByName(site);		 
+		//unknown host exception 
+		System.out.println("\n\nIP address is:"+ip); 
+	} 
 }
--- C#/temp.cs
+++ C#/temp.cs
@@ -1,57 +1,58 @@
-/*This is a testing file*/
 using System;
 
-public class temp
+//base class
+class b
+{
+	int bx;
+	public b()
+	{
+		Console.WriteLine("Default base");
+	}
+	public b(int val)
+	{
+		bx=val;
+		Console.WriteLine("Base with value");
+	}
+	public void display()
+	{
+		Console.WriteLine("bx= "+bx );
+	}	
+}
+
+//derived class 
+class d:b
+{
+	int dx;
+	public d()
+	{
+		Console.WriteLine("Default derived");
+	}
+	public d(int val)
+	{
+		dx=val;
+		Console.WriteLine("Derived with value");
+	}
+	public new void display()
+	{
+		Console.WriteLine("dx= "+dx );
+	}	
+}
+
+class demo
 {
 	public static void Main()
 	{
-		Console.WriteLine("Enter Decimal No.:::");
-		double n=double.Parse(Console.ReadLine());
-		int mul=1,r=2,temp=0;
-		double sum=0;
-			//integer part
-		while(n!=0)
-		{
-			temp=(int)n%2;
-			n=n/r;
-			sum+=temp;
-			mul*=10;
-		}
-			//decimal part
-		while(false)
-		{
-			
-		}
-		Console.Write(sum);
+		b b1=new b(2);
+		b1.display();
+		
+		d d1=new d(3);
+		d1.display();
+		
+		b b2=new d(5);
+		b2.display();
+		
+		d d2=new d();
+		d2.display();
 	}
 }
 
-
-
-
-
-// Console.WriteLine("Enter binary no.:::");
-		// double n=double.Parse(Console.ReadLine());
-		// int c1=-1,c2=0,r=2;double sum=0;
-		// double y=n%1;
-			
-			// //integer part
-		// while(n!=0)
-		// {
-			
-			// c1++;
-			// int x=(int)n%10; 	//last digit
-			// n/=10;			//number changed
-			// sum+=(x*Math.Pow(r,c1));
-		
-		// }
-			// //decimal part
-		// while(y!=0)
-		// {
-			// c2++;	//index
-			// y*=10;
-			// int x=(int)y/1;	//division
-			// y%=1;		//update y
-			// sum+=x*(int)Math.Pow(r,-c2);			
-		// }
-		// Console.Write(sum);
