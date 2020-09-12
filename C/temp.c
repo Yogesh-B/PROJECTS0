ff8e32c15f546ae0b95256c151cfede84529d7be
Yogesh-B <yogeshbavishi199@gmail.com>
2020-09-12 20:43:02 +0530

    testing add

--- C/temp.c
+++ C/temp.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-#include <regex.h>
 int main()
 {
-	printf("\n\nhello world!!!\n\n");
-	return 0;
+	int i,j,sum;
+	int counter=0, arr[7]={1,2,3,4,5,6,7};
+
+	for ( i=0; i<3; i++ ){
+		for ( j=0; j<3; j++ ){
+			if ( (i+j)%2 == 0 || i == 0 || i == 2 ){
+				printf("%d", arr[counter]);
+				counter++;
+			}
+		}
+		printf("\n");
+	}	
+
+
+	 
+    return 0; 
 }
